b448c2980e7c4abaee665d5c634a0c9f68d470df
Kyle Melton <87396239+KyleMel@users.noreply.github.com>
2021-09-16 20:31:53 -0500

    "Corrects LinkedList Begin Demo 1 and 2"
    All the example work was orignally done in 02_02 Begin.
    (refer to prior commits and comments for verification)
    Adds code to the rest of 02 Begin examples for the sake
    of completion.

--- Ch02/02_02/Begin/LinkedList/Program.cs
+++ Ch02/02_02/Begin/LinkedList/Program.cs
@@ -10,25 +10,17 @@ namespace SinglyLinkedList
     {
         static void Main(string[] args)
         {
-            SinglyLinkedList myList = new SinglyLinkedList();
-            myList.insertFirst(100);
-            myList.insertFirst(50);
-            myList.insertFirst(99);
-            myList.insertFirst(88);
-            myList.insertLast(999000);
-            myList.displayList();
-
-            myList.deleteFirst();
-            myList.displayList();
+            //  TODO
         }
     }
     public class SinglyLinkedList
     {
         private Node first;
-        public bool isEmtpy()
+        public bool isEmpty()
         {
             return (first == null);
         }
+
         public void insertFirst(int data)
         {
             Node newNode = new Node();
@@ -36,36 +28,17 @@ namespace SinglyLinkedList
             newNode.next = first;
             first = newNode;
         }
+
         public Node deleteFirst()
         {
             Node temp = first;
             first = first.next;
             return temp;
         }
-        public void displayList()
-        {
-            Console.WriteLine("List (first -- last) ");
-            Node current = first;
-            while (current != null)
-            {
-                current.displayNode();
-                current = current.next;
-            }
-            Console.WriteLine();
-        }
-        public void insertLast(int data)
-        {
-            Node current = first;
-            while (current.next != null)
-            {
-                current = current.next;
-            }
-            Node newNode = new Node();
-            newNode.data = data;
-            current.next = newNode;
-        }
+
     }
-    public class Node 
+
+    public class Node
     {
         public int data;
         public Node next;
@@ -75,5 +48,4 @@ namespace SinglyLinkedList
             Console.WriteLine("<" + data + ">");
         }
     }
-
 }
--- Ch02/02_03/Begin/LinkedList/Program.cs
+++ Ch02/02_03/Begin/LinkedList/Program.cs
@@ -10,7 +10,16 @@ namespace SinglyLinkedList
     {
         static void Main(string[] args)
         {
-            //  TODO
+            SinglyLinkedList myList = new SinglyLinkedList();
+            myList.insertFirst(100);
+            myList.insertFirst(50);
+            myList.insertFirst(99);
+            myList.insertFirst(88);
+            myList.insertLast(999000);
+            myList.displayList();
+
+            myList.deleteFirst();
+            myList.displayList();
         }
     }
     public class SinglyLinkedList
@@ -36,6 +45,30 @@ namespace SinglyLinkedList
             return temp;
         }
 
+        public void displayList()
+        {
+            Console.WriteLine("List (first -- last) ");
+            Node current = first;
+            while (current != null)
+            {
+                current.displayNode();
+                current = current.next;
+            }
+            Console.WriteLine();
+        }
+
+        public void insertLast(int data)
+        {
+            Node current = first;
+            while (current.next != null)
+            {
+                current = current.next;
+            }
+
+            Node newNode = new Node();
+            newNode.data = data;
+            current.next = newNode;
+        }
     }
 
     public class Node
