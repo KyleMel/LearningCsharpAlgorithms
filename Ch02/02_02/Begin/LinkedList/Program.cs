7af83fb2bf862d3d9de7203de23002f8b01df406
Kyle Melton <87396239+KyleMel@users.noreply.github.com>
2021-09-16 20:14:05 -0500

    Link list demo, part2

--- Ch02/02_02/Begin/LinkedList/Program.cs
+++ Ch02/02_02/Begin/LinkedList/Program.cs
@@ -10,10 +10,19 @@ namespace SinglyLinkedList
     {
         static void Main(string[] args)
         {
-            //TODO
+            SinglyLinkedList myList = new SinglyLinkedList();
+            myList.insertFirst(100);
+            myList.insertFirst(50);
+            myList.insertFirst(99);
+            myList.insertFirst(88);
+            myList.insertLast(999000);
+            myList.displayList();
+
+            myList.deleteFirst();
+            myList.displayList();
         }
     }
-    public class SingleLinkedList
+    public class SinglyLinkedList
     {
         private Node first;
         public bool isEmtpy()
@@ -32,6 +41,28 @@ namespace SinglyLinkedList
             Node temp = first;
             first = first.next;
             return temp;
+        }
+        public void displayList()
+        {
+            Console.WriteLine("List (first -- last) ");
+            Node current = first;
+            while (current != null)
+            {
+                current.displayNode();
+                current = current.next;
+            }
+            Console.WriteLine();
+        }
+        public void insertLast(int data)
+        {
+            Node current = first;
+            while (current.next != null)
+            {
+                current = current.next;
+            }
+            Node newNode = new Node();
+            newNode.data = data;
+            current.next = newNode;
         }
     }
     public class Node 
